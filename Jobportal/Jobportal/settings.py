5f0321cdd594853decc97f15dc9f9a13504c82de
athulunni7-git <athulunnikrishnan.in@gmail.com>
2026-08-06 13:40:59 +0530

    production settings

--- Jobportal/Jobportal/settings.py
+++ Jobportal/Jobportal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h5dhxdyl)h#y4g%(!z3qw=i4j@^#8f2fi6w=@wnh1mxe=fel5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
